e7d1d2992bbb4dbe306fdfa60349c176749cfecd
jahorta <jahorta00000@gmail.com>
2025-05-26 02:53:57 -0500

    MemWatchModel: On collapsing an array node, need to add a placeholder entry

--- Source/GUI/MemWatcher/MemWatchModel.cpp
+++ Source/GUI/MemWatcher/MemWatchModel.cpp
@@ -1008,6 +1008,9 @@ void MemWatchModel::collapseArrayNode(MemWatchTreeNode* node)
 {
   while (node->hasChildren())
     deleteNode(getIndexFromTreeNode(node->getChildren()[0]));
+
+  addNodes({new MemWatchTreeNode(new MemWatchEntry(m_placeholderEntry))},
+           getIndexFromTreeNode(node), true);
 }
 
 int MemWatchModel::getTotalContainerLength(MemWatchEntry* entry)
