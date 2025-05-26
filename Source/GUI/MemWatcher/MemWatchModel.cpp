6cc52011924cd3caafc5c75c3d39c837585fd51a
jahorta <jahorta00000@gmail.com>
2025-05-26 02:52:54 -0500

    MemWatchModel: Added feedback for array status in value column

--- Source/GUI/MemWatcher/MemWatchModel.cpp
+++ Source/GUI/MemWatcher/MemWatchModel.cpp
@@ -417,6 +417,10 @@ QVariant MemWatchModel::data(const QModelIndex& index, int role) const
           else
             return QString();
         }
+        else if (entry->getType() == Common::MemType::type_array)
+        {
+          return QString("%1 entries in array.").arg(entry->getContainerCount());
+        }
         break;
       }
       default:
