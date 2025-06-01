8b9b8f978f352e64d6836d2367dccffb872afba4
jahorta <jahorta00000@gmail.com>
2025-06-01 09:12:43 -0500

    formatting

--- Source/GUI/StructEditor/StructDetailModel.cpp
+++ Source/GUI/StructEditor/StructDetailModel.cpp
@@ -684,8 +684,7 @@ int StructDetailModel::getTotalContainerLength(MemWatchEntry* entry)
            entry->getType() != Common::MemType::type_struct)
     return Common::getSizeForType(entry->getType(), entry->getLength());
   else if (entry->getType() == Common::MemType::type_array && entry->getContainerEntry() != nullptr)
-    return entry->getContainerCount() *
-           getTotalContainerLength(entry->getContainerEntry());
+    return entry->getContainerCount() * getTotalContainerLength(entry->getContainerEntry());
   else if (entry->getType() == Common::MemType::type_struct)
   {
     int len = 0;
